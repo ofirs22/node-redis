3b27b05dabc3cee4af34d347f5dcb7b3a59d0608
ofirs22 <75007980+ofirs22@users.noreply.github.com>
2024-04-25 17:25:03 +0300

    add connect event and remove the extra instance

--- server.js
+++ server.js
@@ -4,32 +4,13 @@ const port = 8080;
 const Redis = require('ioredis');
 
 
-const sentinelConfig = {
-  sentinels: [
-      { host: 'redis-sentinel-node-0.redis-sentinel-headless.redis.svc.cluster.local', port: 26379 },
-      { host: 'redis-sentinel-node-1.redis-sentinel-headless.redis.svc.cluster.local', port: 26379 },
-      // Add more Sentinel instances if needed
-  ],
-  name: 'mymaster', // Replace 'mymaster' with your master name
-  password: 'M4MDvf4AQwgbwX6Y',
-  sentinelPassword: 'M4MDvf4AQwgbwX6Y',
-};
 
-const subClient = new Redis(sentinelConfig)
 
 const redisClient = require('./app/config/redis.config');
 
-// Subscribe to the __keyspace@0__:connect channel to monitor client connections
-subClient.subscribe('__keyspace@0__:connect');
-
-// Listen for messages on the subscribed channel
-subClient.on('message', (channel, message) => {
-    if (channel === '__keyspace@0__:connect') {
-        console.log('Client connected:', message);
-        // Here, 'message' will be the client's IP address or identifier
-        // You can add more logic to handle the connection event
-    }
-});
+redisClient.on('connect', () => {
+  console.log('Connected to Redis server');
+})
 
 
 app.use(express.json());
